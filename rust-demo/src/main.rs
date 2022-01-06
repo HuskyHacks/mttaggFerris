8d4c1458f3f008050de49c9d50afdf9bcd6cff73
matt <huskyhacks.mk@gmail.com>
2022-01-05 21:08:21 -0500

    so far so good

--- rust-demo/src/main.rs
+++ rust-demo/src/main.rs
@@ -1,6 +1,13 @@
 // fn is the function declarer
 
 fn main() {
+    
+    
+    
+    // conditionals
+    // A Vec is like a list in Python 
+    //let names: Vec<&str> = vec!["Tag", "Husky", "D0nut", "GoPro"]
+    
     // var keyword is 'let' like in Nim   
     // let is immutable, but there is no "mutable" variable like var
     // if you want to have a changable var, you need to give it the mut keyword
@@ -8,8 +15,14 @@ fn main() {
     // there is also String type
     let name: &str = "Husky";
     
-    println!("Hello, {}", name);
+    if name == "Husky" {
+        println!("Oh hai guy");
+    } else if name == "Taggart" {
+        println!("Hey Tag :)")
+    } else {
+        println!("Hello, {}", name);
     // println is actually a macro (meta programming)
     // builds new code to perform a given function
+    }
 }
 
